966a6045470eefc66f06a10447d039f11726b519
Rajat Anantharam <rajat@funda.nl>
2020-11-05 10:41:15 +0100

    AuthorizationService is now public

--- library/java/net/openid/appauth/AuthorizationService.java
+++ library/java/net/openid/appauth/AuthorizationService.java
@@ -58,7 +58,7 @@ public class AuthorizationService {
      * Constructor that injects a url builder into the service for testing.
      */
     @VisibleForTesting
-    AuthorizationService(@NonNull Context context,
+    public AuthorizationService(@NonNull Context context,
                          @NonNull AppAuthConfiguration clientConfiguration) {
         mContext = checkNotNull(context);
         mClientConfiguration = clientConfiguration;
